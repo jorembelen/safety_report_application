6df022a4873ea44fb9382fc691b15faf5b2fd655
Jorem Belen <jorembelen@gmail.com>
2022-08-20 17:26:58 +0300

    login updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>HSE APP Login Page</title>
+    <title>Safety APP | Login Page</title>
     {{-- <link rel="icon" type="image/x-icon" href="/admin/assets/img/rcl.ico"/> --}}
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="/admin/https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&amp;display=swap" rel="stylesheet">
